5185c3ef44fbb0583ae4ffeefcce9e9efa7c2c31
Fredrik Siemund <fredrik.siemund@gmail.com>
2022-07-17 14:38:18 +0200

    Add infinite scroll

--- lib/character_list.dart
+++ lib/character_list.dart
@@ -17,32 +17,70 @@ class CharacterList extends StatefulWidget {
 }
 
 class _CharacterListState extends State<CharacterList> {
-  late Future<List<Character>> _futureCharacters;
-  late String _nextPage;
+  final controller = ScrollController();
+  List<Character> characters = [];
+  String nextPage = 'https://rickandmortyapi.com/api/character';
+  bool hasMore = true;
+  bool isLoading = false;
 
-  Future<List<Character>> fetchCharacters(String url) async {
-    final res = await http.get(Uri.parse(url));
+  Future fetch() async {
+    if (isLoading) return;
+
+    setState(() {
+      isLoading = true;
+    });
+
+    final url = Uri.parse(nextPage);
+    final res = await http.get(url);
 
     if (res.statusCode == 200) {
       Map<String, dynamic> parsedRes = jsonDecode(res.body);
-      _nextPage = parsedRes['info']['next'];
-      return List<Character>.from(
-          parsedRes['results'].map((entry) => Character.fromJson(entry)));
+      String? nextPageRaw = parsedRes['info']['next'];
+      setState(() {
+        if (nextPageRaw == null) {
+          hasMore = false;
+        } else {
+          nextPage = nextPageRaw;
+        }
+
+        characters.addAll(List<Character>.from(
+            parsedRes['results'].map((entry) => Character.fromJson(entry))));
+
+        isLoading = false;
+      });
     } else {
       throw Exception('Failed to load character');
     }
   }
 
-  // void fetchMoreCharacters() async {
-  //   final moreCharacters = await fetchCharacters(await _nextPage);
-  //   _futureCharacters = _futureCharacters + moreCharacters;
-  // }
+  Future refresh() async {
+    setState(() {
+      characters = [];
+      nextPage = 'https://rickandmortyapi.com/api/character';
+      hasMore = true;
+      isLoading = false;
+    });
+
+    fetch();
+  }
 
   @override
   void initState() {
     super.initState();
-    _futureCharacters =
-        fetchCharacters('https://rickandmortyapi.com/api/character');
+
+    fetch();
+
+    controller.addListener(() {
+      if (controller.position.maxScrollExtent == controller.offset && hasMore) {
+        fetch();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -51,40 +89,36 @@ class _CharacterListState extends State<CharacterList> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: FutureBuilder<List<Character>>(
-        future: _futureCharacters,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-              padding: const EdgeInsets.all(16.0),
-              itemCount: snapshot.data!.length * 2 + 1,
-              itemBuilder: (context, i) {
-                if (i.isOdd) return const Divider();
+      body: RefreshIndicator(
+        onRefresh: refresh,
+        child: ListView.builder(
+          controller: controller,
+          padding: const EdgeInsets.all(16.0),
+          itemCount: characters.length * 2 + 1,
+          itemBuilder: (context, i) {
+            if (i.isOdd) return const Divider();
 
-                final index = i ~/ 2;
-                if (index >= snapshot.data!.length) {
-                  return const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 16),
-                    child: Center(child: CircularProgressIndicator()),
-                  );
-                }
-
-                return ListTile(
-                  leading: Image.network(snapshot.data![index].image),
-                  title: Text(snapshot.data![index].name),
-                  subtitle: Text(snapshot.data![index].species),
-                  onTap: () =>
-                      Navigator.of(context).pushNamed('character-details'),
-                );
-              },
-            );
-          } else if (snapshot.hasError) {
-            return Text('${snapshot.error}');
-          }
-
-          // By default, show a loading spinner.
-          return const Center(child: CircularProgressIndicator());
-        },
+            final index = i ~/ 2;
+            if (index < characters.length) {
+              return ListTile(
+                leading: Image.network(characters[index].image),
+                title: Text(characters[index].name),
+                subtitle: Text(characters[index].species),
+                onTap: () =>
+                    Navigator.of(context).pushNamed('character-details'),
+              );
+            } else {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                child: Center(
+                  child: hasMore
+                      ? const CircularProgressIndicator()
+                      : const Text('No more data to load'),
+                ),
+              );
+            }
+          },
+        ),
       ),
     );
   }
